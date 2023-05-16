4b8b8604b520de0f76543d90c3b492e8a17c5dd0
Joshua Trono <Josh.trono@gmail.com>
2023-05-16 00:11:28 -0400

    did problem b.

--- 5-15-week/LoopDetection.rb
+++ 5-15-week/LoopDetection.rb
@@ -1,1 +1,42 @@
-def main 
+require 'linked-list'
+
+node1 = LinkedList::Node.new("10")
+node2 = LinkedList::Node.new("20")
+node3 = LinkedList::Node.new("30")
+
+test_list = LinkedList::List.new
+test_list.push("1")
+test_list.push(node2)
+
+list2 = LinkedList::List.new
+list2.push("1")
+list2.push("2")
+list2.push(node3)
+list2.push(node1)
+list2.push("3")
+list2.push(node3)
+
+
+
+# goes through the list and if a value references a value already. return and break the loop.
+def find_duplicate_reference(linked_list)
+    references = []
+    
+    linked_list.each do |node|
+      if references.include?(node)
+        puts "The duplicate value is: #{node}"
+        references = []
+        return node
+      end
+      
+      references.push(node)
+    end
+    
+    puts "No duplicate value found."
+    nil
+  end
+  
+  
+find_duplicate_reference(test_list)
+find_duplicate_reference(list2)
+
