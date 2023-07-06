09d396fbdd0047271e89a044d940a9589d728cf4
Joshua Trono <josh.trono@gmail.com>
2023-07-06 12:30:29 -0400

    finished the hack n fix

---
+++ day_7_6/MaxNumberFinder.java
@@ -0,0 +1,19 @@
+package day_7_6;
+
+public class MaxNumberFinder {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {28, 5, 25, 8, 15};
+
+        int max = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("Maximum number is: " + max);
+    }
+
+}
